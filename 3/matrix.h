f71aa328210d61a7c8b379cb72e2db9dfad43809
Dodonov Alexandr <31758180+Dodon4@users.noreply.github.com>
2019-03-26 21:23:15 +0300

    Update matrix.h

--- 3/matrix.h
+++ 3/matrix.h
@@ -1,1 +1,98 @@
+class ProxyM {
+private:
+	size_t columns;
+	int* data_;
+public:
+	ProxyM(const size_t col) :columns(col) {
+		data_ = new int[columns];
+	}
+	~ProxyM() {
+			delete[] data_;
+	}
+	const int& operator[](size_t i) const {
+				if (i >= columns) 
+					throw std::out_of_range("");
+				return data_[i];
+	}
+	int& operator[](size_t i) {
+		if (i >= columns)
+			throw std::out_of_range("");
+		return data_[i];
+	}
+	ProxyM& operator*=(const int& other) {
+		for (size_t i = 0; i < columns; ++i)
+			data_[i] *= other;
+		return *this;
+	}
+
+	bool operator==(const ProxyM& other) const {
+		if (this == &other)
+			return true;
+		if (columns != other.columns) 
+			return false;
+		for (size_t i = 0; i < columns; ++i)
+			if (data_[i] != other.data_[i])
+				return false;
+		return true;
+	}
+	bool operator!=(const ProxyM& other) const {
+		return !(*this == other);
+	}
+};
+class Matrix {
+private:
+	size_t rows, columns;
+	ProxyM* data_;
+public:
+	Matrix(const size_t row, const size_t col) : rows(row), columns(col) {
+		data_ = static_cast<ProxyM*>(operator new[](sizeof(ProxyM)*rows));
+		for (size_t i = 0; i < rows; ++i) {
+			new (data_ + i) ProxyM(col);
+		}
+	}
+	~Matrix() {
+		for (size_t i = 0; i < rows; ++i) {
+			data_[i].~ProxyM();
+		}
+		operator delete[](data_);
+	}
+	const ProxyM& operator[](size_t i) const {
+		if (i >= rows) 
+			throw std::out_of_range("");
+		return data_[i];
+	}
+	ProxyM& operator[](size_t i) {
+		if (i >= rows) 
+			throw std::out_of_range("");
+		return data_[i];
+	}
+
+	Matrix& operator *=(const int other) {
+		for (size_t i = 0; i < rows; ++i) 
+			data_[i] *= other;
+		return *this;
+	}
+	bool operator==(const Matrix& other) const {
+		if (this == &other)
+			return true;
+		if (rows != other.rows || columns != other.columns) {
+			return false;
+		}
+		for (size_t i = 0; i < rows; ++i)
+			if (data_[i] != other.data_[i])
+				return false;
+		return true;
+		}
+
+
+		bool operator!=(const Matrix& other) const {
+			return !(*this == other);
+		}
+	int getRows() {
+		return rows;
+	}
+	int getColumns() {
+		return columns;
+	}
+};
 
